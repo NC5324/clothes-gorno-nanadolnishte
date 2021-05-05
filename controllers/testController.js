99869dd31840ea3dbb61249ce405d88e118ebfa9
NaidDzhan Durmush <nc.durmush@gmail.com>
2021-05-06 00:09:12 +0300

    golqma ka6a

--- controllers/testController.js
+++ controllers/testController.js
@@ -1,8 +1,7 @@
 import express from 'express'
 const router =express.Router()
 
-router.post('/test', (req, res) => {
-    console.log(req.body)
+router.get('/test', (req, res) => {
     res.status(200).send('Polu4ihme va6eto syob6tenie')
 })
 
