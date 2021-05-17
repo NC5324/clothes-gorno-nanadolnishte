9335562df1fee69637c9ed4f4eca544668db78d4
NaidDzhan Durmush <nc.durmush@gmail.com>
2021-05-18 00:36:30 +0300

    add close button to cart add totalPrice to cart

--- public/scripts/script-global.js
+++ public/scripts/script-global.js
@@ -37,55 +37,64 @@ $(document).ready(async() => {
         window.location.href = `/browse.html`
     })
 
-    document.getElementById('cart-button').addEventListener('click', () => {
-        const cart = document.getElementById('cart')
-        cart.style.display = (cart.style.display === 'flex' ? 'none' : 'flex')
+    const cartBtn = document.getElementById('cart-button')
+    if(cartBtn) {
+        cartBtn.addEventListener('click', () => {
+            const cart = document.getElementById('cart')
+            cart.style.display = (cart.style.display === 'flex' ? 'none' : 'flex')
 
-        const cartItems = JSON.parse(localStorage.getItem('CART_ITEMS'))
-        if(cartItems) {
-            document.querySelector('.cart-items main').innerHTML = ''
-            Object.keys(cartItems).forEach(key => {
-                const clothing = cartItems[key].item
-                const clone = document.getElementById('template-cart-item').content.cloneNode(true)
-                clone.querySelector('h2').textContent = clothing.title
+            const cartItems = JSON.parse(localStorage.getItem('CART_ITEMS'))
+            let totalPrice = 0
+            if(cartItems) {
+                document.querySelector('.cart-items main').innerHTML = ''
+                Object.keys(cartItems).forEach(key => {
+                    const clothing = cartItems[key].item
+                    const clone = document.getElementById('template-cart-item').content.cloneNode(true)
+                    clone.querySelector('h2').textContent = clothing.title
 
-                //Adjust thumbnail
-                clone.querySelector('img')
-                    .setAttribute('src', (clothing.Images.length > 0 ? clothing.Images[0].path : 'assets/placeholder.jpg'))
+                    //Adjust thumbnail
+                    clone.querySelector('img')
+                        .setAttribute('src', (clothing.Images.length > 0 ? clothing.Images[0].path : 'assets/placeholder.jpg'))
 
-                //Add prices of the clothing
-                //Display old price and discount only if there is a discount
-                const discountCondition = clothing.price2 && (clothing.price < clothing.price2)
-                const comparisonPrice = clone.querySelector('h3:first-of-type')
-                const discountTag = clone.querySelector('.promotion')
-                if(discountCondition) {
-                    comparisonPrice.innerHTML = `${clothing.price2} <span class="currency">лв.</span>`
-                    discountTag.querySelector('span').textContent = `-${100 - Math.floor(clothing.price / clothing.price2 * 100)}%`
-                } else {
-                    comparisonPrice.style.display = 'none'
-                    discountTag.style.display = 'none'
-                }
-                //Add price
-                clone.querySelector('h3:last-of-type').innerHTML = `${clothing.price} <span class="currency">лв.</span>`
+                    //Add prices of the clothing
+                    //Display old price and discount only if there is a discount
+                    const discountCondition = clothing.price2 && (clothing.price < clothing.price2)
+                    const comparisonPrice = clone.querySelector('h3:first-of-type')
+                    const discountTag = clone.querySelector('.promotion')
+                    if(discountCondition) {
+                        comparisonPrice.innerHTML = `${clothing.price2} <span class="currency">лв.</span>`
+                        discountTag.querySelector('span').textContent = `-${100 - Math.floor(clothing.price / clothing.price2 * 100)}%`
+                    } else {
+                        comparisonPrice.style.display = 'none'
+                        discountTag.style.display = 'none'
+                    }
+                    //Add price
+                    clone.querySelector('h3:last-of-type').innerHTML = `${clothing.price} <span class="currency">лв.</span>`
+                    totalPrice += clothing.price
 
-                //Add data-id attribute that contains the ID of the clothing
-                clone.querySelector('article').setAttribute('data-id', clothing.id)
+                    //Add data-id attribute that contains the ID of the clothing
+                    clone.querySelector('article').setAttribute('data-id', clothing.id)
 
-                //Add link to product details page
-                clone.querySelector('article').addEventListener('click', () => {
-                    localStorage.setItem('PRODUCT_ID', clothing.id)
-                    window.location.href = `/details.html`
+                    //Add link to product details page
+                    clone.querySelector('article').addEventListener('click', () => {
+                        localStorage.setItem('PRODUCT_ID', clothing.id)
+                        window.location.href = `/details.html`
+                    })
+
+                    document.querySelector('.cart-items main').appendChild(clone)
                 })
-
-                document.querySelector('.cart-items main').appendChild(clone)
-            })
-        }
-
-    })
+            }
+            document.querySelector('.cart-content footer h3:last-of-type').textContent = `${totalPrice} лв.`
+        })
+    }
 
     document.getElementById('cart').addEventListener('click', (ev) => {
         if(ev.target === ev.currentTarget) {
             ev.currentTarget.style.display = 'none'
         }
+    })
+
+    document.getElementById('exit-cart-button').addEventListener('click', () => {
+        document.getElementById('cart').style.display = 'none'
     })
 })
